17db05bc7f84d9a9c43e5fad2c8205abcf6ffb1d
Aurelienbre <aurelien.bresciani@gmail.com>
2025-08-25 12:06:17 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+Module des routes de l'API de planification
+
+Ce package contient tous les routers FastAPI pour organiser les endpoints :
+- beta_api_routes.py : Routes pour la gestion des chantiers et préparateurs (Beta-API.html)  
+- grille_semaine_routes.py : Routes pour la gestion des étiquettes et horaires (Grille semaine.html)
+"""
+
+__version__ = "2.0.0"
